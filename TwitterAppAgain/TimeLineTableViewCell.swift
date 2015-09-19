3c137bb3f172c37f540a67fb58b32a90ac582765
あゆみ <ayuyumimi03@gmail.com>
2015-09-19 17:12:20 +0900

    リネーム

---
+++ TwitterAppAgain/TimeLineTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TimeLineTableViewCell.swift
+//  TwitterAppAgain
+//
+//  Created by あゆみ on 2015/09/19.
+//  Copyright (c) 2015年 あゆみ. All rights reserved.
+//
+
+import UIKit
+
+class TimeLineTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
