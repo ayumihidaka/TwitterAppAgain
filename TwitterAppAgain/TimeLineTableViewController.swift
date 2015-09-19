7969174dea6909007e57bdfd209dc86906401ec8
あゆみ <ayuyumimi03@gmail.com>
2015-09-19 18:04:52 +0900

    タイムライン一覧にダミー画像追加

--- TwitterAppAgain/TimeLineTableViewController.swift
+++ TwitterAppAgain/TimeLineTableViewController.swift
@@ -9,8 +9,12 @@
 import UIKit
 
 class TimeLineTableViewController: UITableViewController {
-    
-    let dataArray:[[String:String]] = [["title":"タイトル1"],["title":"タイトル2"]]
+    //https://pbs.twimg.com/profile_images/459921170251264000/ax4FMwXA.jpeg
+    //http://pic.prepics-cdn.com/4a32cbc7bdf5/8064422.gif
+    let dataArray:[[String:String]] = [
+        ["title":"タイトル1","image":"http://blog-imgs-56.fc2.com/r/o/e/roenvy/2014112013153672a.gif"],
+        ["title":"タイトル2","image":"http://pds.exblog.jp/logo/1/200509/01/93/e006369320070720115807.gif"]
+    ]
     
     //テーブルの件数を登録
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -24,6 +28,14 @@ class TimeLineTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! TimeLineTableViewCell
         
         cell.tweetLabel.text = dataArray[indexPath.row]["title"]
+        
+        
+        if let imageURLString = dataArray[indexPath.row]["image"]
+        ,let imageURL = NSURL(string: imageURLString){
+            
+        cell.iconimageView.sd_setImageWithURL(imageURL)
+            
+        }
         
         return cell
     }
